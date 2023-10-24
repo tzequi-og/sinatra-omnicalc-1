7f71f0335d461ad9cd16eafcab644f4198ef55fb
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-24 16:51:16 +0000

    sq_sqrt

--- app.rb
+++ app.rb
@@ -11,9 +11,23 @@ get("/square/new") do
   erb(:square, { :layout => :layout })
 end
 
+get("/square/results") do
+  @number = params.fetch(:number)
+  @square = @number.to_f ** 2
+
+  erb(:square_results, { :layout => :layout })
+end
+
 get("/square_root/new") do
   
   erb(:square_root, { :layout => :layout })
+end
+
+get("/square_root/results") do
+  @number = params.fetch(:number)
+  @square_root = @number.to_f ** 0.5
+
+  erb(:square_root_results, { :layout => :layout })
 end
 
 get("/payment/new") do
